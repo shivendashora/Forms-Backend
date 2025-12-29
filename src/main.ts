3cb14622741552d34a739d8dc67eceebcb106d7f
Shiven Dashora <shiven.dashora@gmail.com>
2025-12-29 22:24:29 +0530

    added the port to access all over

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3001', 
+    origin: ['http://localhost:3001', 'http://16.171.172.196:3001'], // Add your public URL
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
 
-  await app.listen(3000); 
+  await app.listen(3000, '0.0.0.0'); 
 }
 bootstrap();
